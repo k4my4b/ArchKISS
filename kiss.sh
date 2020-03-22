af0d735f4aac934120c60b7d00897c1b6322665e
Kamyab Sherafat <k4my4b@gmail.com>
2020-03-22 03:45:07 +0000

    added base installation and package installation. now we just add a package name to the list and voila !

--- kiss.sh
+++ kiss.sh
@@ -22,18 +22,44 @@ STAGES=(
     update_mirrors "Updating mirrorlist ..."
     #8- enable ntp to automatically update the time and date
     enable_ntp "Enabling Network Time Protocol ..."
+    #9- install the base packages first
+    install_base "Installing base packages ..."
+    #10- install all the remaining packages from the PACKAGES list one by one
+    # this in itself doesn't have a status update but each package does
+    install_pac ""
 )
 
-# list of all packages to be installed
-PACKAGEES=(
+# base packages
+PACKAGES_BASE=(
+    base
+    base-devel
+    linux
+    linux-headers
+    linux-firmware
+)
+
+# list of all other packages to be installed
+PACKAGES=(
+    # Nvidia stuff
+    nvidia-dkms
+    nvidia-settings
+    ffnvcodec-headers
+    libvdpau
+    libxnvctrl
+    nvidia-utils
+    nvtop
+    # Xorg stuff
+    xorg
+    xinit
+    # KDE plasma
 )
 
 # where to save the logs ?
-LOG_FIEL="install.log"
+LOG_FILE="install.log"
 LOG_DELIMITER="#-----------------------------------------------------------"
 
 # default instllation route
-MNT="/mnt"
+MNT="/tmp"
 
 # COLORS
 RED="\e[91m"
@@ -69,72 +95,99 @@ check_root() {
 
 # make sure we are connected
 check_connection() {
-    if ! ping -c 3 google.com 1>> $LOG_FIEL 2>> $LOG_FIEL; then
+    if ! ping -c 3 google.com 1>>$LOG_FILE 2>>$LOG_FILE; then
         return 1
     fi
 }
 
 # get pacman started
 update_pacman() {
-    if ! pacman -Syy 1>> $LOG_FIEL 2>> $LOG_FIEL; then
+    if ! pacman -Syy 1>>$LOG_FILE 2>>$LOG_FILE; then
         return 1
     fi
 }
 
 # install reflector to get the mirrorlist updated, we don't want this installation to take forever
 install_reflector() {
-    if ! pacman -S --needed --noconfirm reflector 1>> $LOG_FIEL 2>> $LOG_FIEL; then
+    if ! pacman -S --needed --noconfirm reflector 1>>$LOG_FILE 2>>$LOG_FILE; then
         return 1
     fi
 }
 
 # find location/country of user to pass to reflector
 find_country() {
-    if ! COUNTRY=$(whois $(curl -sSL ifconfig.me) | grep -iE -m1 ^country: | cut -d ' ' -f9) 1>> $LOG_FIEL 2>> $LOG_FIEL; then
+    if ! COUNTRY=$(whois $(curl -sSL ifconfig.me) | grep -iE -m1 ^country: | cut -d ' ' -f9) 1>>$LOG_FILE 2>>$LOG_FILE; then
         return 1
     fi
 }
 
 # with a bit of luck we should we able to find some better mirrors around
 update_mirrors() {
-    if ! (reflector -c GB --sort score --threads $(nproc) --save $MIRRORS_URI) 1>> $LOG_FIEL 2>> $LOG_FIEL; then
+    if ! (reflector -c GB --sort score --threads $(nproc) --save $MIRRORS_URI) 1>>$LOG_FILE 2>>$LOG_FILE; then
         return 1
     fi
 }
 
 # enable network time sync
 enable_ntp() {
-    if ! timedatectl set-ntp true 1>> $LOG_FIEL 2>> $LOG_FIEL; then
+    if ! timedatectl set-ntp true 1>>$LOG_FILE 2>>$LOG_FILE; then
         return 1
     fi
 }
 
+# install the base system
+install_base() {
+    if ! pacstrap $MNT ${PACKAGES_BASE[@]} 1>>$LOG_FILE 2>>$LOG_FILE; then
+        return 1
+    fi
+}
+
+# install the rest of the packges this doesm't mean that these are non-essential
+install_pac() {
+    echo -e "\t---------------------------------------"
+    echo -e "\t          Installing Packages          jobsjosasdasdasd"
+    echo -e "\t---------------------------------------"
+    for pac in ${PACKAGES[@]}; do
+        echo -ne "\t${WHITE}☐ $pac${COL_DEFAULT}"
+        if pacstrap $MNT $pac 1>>$LOG_FILE 2>>$LOG_FILE; then
+            echo -ne "\r"
+            echo -e "\t${GREEN}☑ ${WHITE}$pac${COL_DEFAULT}"
+        else
+            echo -ne "\r"
+            echo -e "\t${RED}☒ ${WHITE}$pac${COL_DEFAULT}"
+            echo -e "${RED}Installation failed at the above stage"
+            exit 1
+        fi
+    done
+}
+
 #----------------------------------------------------------------
 #
-#                         HEART OF THINGS
+#                        HERE BE DRAGONS !!!
 #
 #----------------------------------------------------------------
 
-# HERE BE DRAGONS !!!
 # avoid this mess as much as possible, if you can clean it up knock yourself out
 main() {
-    for((n=0;n<${#STAGES[@]}; n+=2));
-    do
-        # make a note of where we are the log file
-        if (( $((n % 2 )) == 0 )); then
-            echo -e "${LOG_DELIMITER}\n# ${STAGES[$n]}\n${LOG_DELIMITER}" >> $LOG_FIEL
+    for ((n = 0; n < ${#STAGES[@]}; n += 2)); do
+        # make a note (in the log file) of where we are now
+        if (($((n % 2)) == 0)); then
+            # echo -ne "${LOG_DELIMITER}\n# \n${LOG_DELIMITER}" >>$LOG_FILE
+            echo -e "-----------------------------------------------------------" >>$LOG_FILE
+            echo -e "\t ${STAGES[$n]}" >>$LOG_FILE
+            echo -e "-----------------------------------------------------------" >>$LOG_FILE
         fi
-        
+
         # if the stage has a description procced with status update
-        if [[ ! -z ${STAGES[$((n+1))]} ]]; then
-            echo -ne "\t${WHITE}  ${STAGES[$((n+1))]}${COL_DEFAULT}"
+        if [[ ! -z ${STAGES[$((n + 1))]} ]]; then
+            echo -ne "\t${WHITE}☐ ${STAGES[$((n + 1))]}${COL_DEFAULT}"
             if ${STAGES[$n]}; then
                 echo -ne "\r"
-                echo -e "\t${GREEN}☑ ${WHITE}${STAGES[$((n+1))]}${COL_DEFAULT}"
-                echo -e ">> no errors encountered\n" >> $LOG_FIEL
+                echo -e "\t${GREEN}☑ ${WHITE}${STAGES[$((n + 1))]}${COL_DEFAULT}"
+                echo -e ">> no errors encountered\n" >>$LOG_FILE
             else
                 echo -ne "\r"
-                echo -e "\t${RED}☒ ${WHITE}${STAGES[$((n+1))]}${COL_DEFAULT}"
+                echo -e "\t${RED}☒ ${WHITE}${STAGES[$((n + 1))]}${COL_DEFAULT}"
                 echo -e "${RED}Installation failed at the above stage"
                 exit 1
             fi
