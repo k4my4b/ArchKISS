e98b095d60458b82b02b84e475b1499393817c29
Kamyab Sherafat <k4my4b@gmail.com>
2020-05-28 15:57:39 +0100

    added aur package installation

--- kiss.sh
+++ kiss.sh
@@ -1,34 +1,40 @@
 #!/usr/bin/env bash
 
-# list of all stages to go through in order
-# altering the order in which each fucntion is called can result in total failure
-# each function is followed by a short description used display status, if
-# the string is empty no status update will be displayed
-# feel free to add to this
+# ---------------------------------------------------------------------------- #
+#                   list of all stages to go through in order                  #
+#    altering the order in which each fucntion is called can result in total   #
+#       failure. Each function is followed by a short description used to      #
+#                            display status message.                           #
+#           If the string is empty no status update will be displayed          #
+#                           feel free to add to this                           #
+# ---------------------------------------------------------------------------- #
+
 STAGES=(
-    #1- print the logo and welcome message
+    # print the logo and welcome message
     print_logo ""
-    #2- make sure we have root privilege
+    # make sure we have root privilege
     check_root "Checking root ..."
-    #3- make sure we have internet connection
+    # make sure we have internet connection
     check_connection "Checking connection ..."
-    #4- update pacman repos
+    # update pacman repos
     update_pacman "Updating Pacman ..."
-    #5- install reflector to update our mirrorlist, we don't want the installation to take forever
+    # install reflector to update our mirrorlist, we don't want the installation to take forever
     install_reflector "Installing Reflector ..."
-    #6- where are we ? where should we look for best servers ?
-    find_country "Determining location ... [${COUNTRY}]"
-    #7- update the mirrorlist accordingly
+    # where are we ? where should we look for best servers ?
+    find_country "Determining location ... ["${COUNTRY}"]"
+    # update the mirrorlist accordingly
     update_mirrors "Updating mirrorlist ..."
-    #8- enable ntp to automatically update the time and date
+    # enable ntp to automatically update the time and date
     enable_ntp "Enabling Network Time Protocol ..."
-    #9- install the base packages first
+    # install the base packages first
     install_base "Installing base packages ..."
-    #10- install all the remaining packages from the PACKAGES list one by one
+    # install all the remaining packages from the PACKAGES list one by one
     # this in itself doesn't have a status update but each package does
     install_pac ""
-    # start installing aur packages
-    install_trizen ""
+    # install a AUR packages
+    install_aur ""
+    # install a bootloader, i.e. Grub
+    install_grub "Installing bootloader (Grub) ..."
 )
 
 # base packages
@@ -57,18 +63,29 @@ PACKAGES=(
     sddm
     # KDE plasma
     plasma-meta
+    # fonts
+    ttf-roboto
+    # <https://wiki.archlinux.org/index.php/Improving_performance#irqbalance> MUST ENABLE THE SERVICE
+    irqbalance
 )
 
 # list of all AUR packages to be installed
 PACKAGES_AUR=(
-    
+    https://aur.archlinux.org/trizen.git
+    https://aur.archlinux.org/kwin-decoration-sierra-breeze-enhanced-git.git
+    https://aur.archlinux.org/latte-dock-git.git
+    https://aur.archlinux.org/ananicy-git.git
+    https://aur.archlinux.org/kwin-lowlatency.git
 )
 
-#----------------------------------------------------------------
-#
-#                          Default Stuff
-#
-#----------------------------------------------------------------
+# list of all services to enable
+SYSTEMD_SERVICES=(
+    irqbalance.service
+)
+
+# ---------------------------------------------------------------------------- #
+#                                 Default Stuff                                #
+# ---------------------------------------------------------------------------- #
 
 # default pacman mirrorlist uri
 MIRRORS_URI="/etc/pacman.d/mirrorlist"
@@ -81,7 +98,6 @@ MNT="/tmp"
 
 # COLORS
 RED="\e[91m"
-WHITE="\e[97m"
 GREEN="\e[92m"
 COL_DEFAULT="\e[39m"
 
@@ -96,11 +112,9 @@ SPACER=" "
 # default intendtaion i.e one tab before status print
 INDENT="\t"
 
-#----------------------------------------------------------------
-#
-#                     Precedures Go Here
-#
-#----------------------------------------------------------------
+# ---------------------------------------------------------------------------- #
+#                              Precedures Go Here                              #
+# ---------------------------------------------------------------------------- #
 
 print_logo() {
     echo -e "\n"
@@ -120,7 +134,7 @@ print_logo() {
 
 # make sure we have root privilege
 check_root() {
-    if ! [ $(id -u) = 0 ] 1>>$LOG_FILE 2>>$LOG_FILE; then
+    if ! [ "$(id -u)" = 0 ] 1>>$LOG_FILE 2>>$LOG_FILE; then
         return 1
     fi
 }
@@ -148,14 +162,14 @@ install_reflector() {
 
 # find location/country of user to pass to reflector
 find_country() {
-    if ! COUNTRY=$(whois $(curl -sSL ifconfig.me) | grep -iE -m1 ^country: | cut -d ' ' -f9) 1>>$LOG_FILE 2>>$LOG_FILE; then
+    if ! COUNTRY="$(whois "$(curl -sSL ifconfig.me)" | grep -iE -m1 ^country: | cut -d ' ' -f9)" 1>>$LOG_FILE 2>>$LOG_FILE; then
         return 1
     fi
 }
 
 # with a bit of luck we should we able to find some better mirrors around
 update_mirrors() {
-    if ! reflector -c ${COUNTRY} --sort score --threads $(nproc) --save $MIRRORS_URI 1>>$LOG_FILE 2>>$LOG_FILE; then
+    if ! reflector -c "${COUNTRY}" --sort score --threads "$(nproc)" --save $MIRRORS_URI 1>>$LOG_FILE 2>>$LOG_FILE; then
         return 1
     fi
 }
@@ -169,80 +183,92 @@ enable_ntp() {
 
 # install the base system
 install_base() {
-    if ! pacstrap $MNT ${PACKAGES_BASE[@]} 1>>$LOG_FILE 2>>$LOG_FILE; then
+    if ! pacstrap "$MNT" "${PACKAGES_BASE[@]}" 1>>$LOG_FILE 2>>$LOG_FILE; then
         return 1
     fi
 }
 
-# install the rest of the packges this doesm't mean that these are non-essential
+# install the rest of the packges this doesm't mean that these are not essential
 install_pac() {
-    echo -e "\t----------------------------------------"
-    echo -e "\t      Installing Official Packages      "
-    echo -e "\t----------------------------------------"
-    for pac in ${PACKAGES[@]}; do
-        echo -ne "${INDENT}${WHITE}${ICO_DEF}${SPACER}${WHITE}$pac${COL_DEFAULT}"
-        if pacstrap ${MNT} $pac 1>>$LOG_FILE 2>>$LOG_FILE; then
+    echo -e "\t"
+    echo -e "\t----- Installing Official Packages -----"
+    echo -e "\t"
+    for pac in "${PACKAGES[@]}"; do
+        echo -ne "${INDENT}${COL_DEFAULT}${ICO_DEF}${SPACER}${COL_DEFAULT}$pac${COL_DEFAULT}"
+        if pacstrap "${ROOT}" "$pac" 1>>$LOG_FILE 2>>$LOG_FILE; then
             echo -ne "\r"
-            echo -e "${INDENT}${GREEN}${ICO_OK}${SPACER}${WHITE}$pac${COL_DEFAULT}"
+            echo -e "${INDENT}${GREEN}${ICO_OK}${SPACER}${COL_DEFAULT}$pac${COL_DEFAULT}"
         else
             echo -ne "\r"
-            echo -e "${INDENT}${RED}${ICO_ERR}${SPACER}${WHITE}$pac${COL_DEFAULT}"
+            echo -e "${INDENT}${RED}${ICO_ERR}${SPACER}${COL_DEFAULT}$pac${COL_DEFAULT}"
             echo -e "${RED}Installation failed at the above stage${COL_DEFAULT}"
             exit 1
         fi
     done
 }
 
-# install trizen from aur using PKGBUILD
-install_trizen() {
-    echo -e "\t----------------------------------------"
-    echo -e "\t          Installing AUR Helper         "
-    echo -e "\t----------------------------------------"
-    if ! (
-        useradd kiss && \ 
-        git clone https://aur.archlinux.org/trizen.git /tmp/trizen && \
-        cd /tmp/trizen && \
-        su kiss && \
-        makepkg -s && \
-        exit && \ 
-        userdel kiss && \
-        pacman --root ${MNT} -U trizen-*.pkg.tar.xz 
-    ) 1>>$LOG_FILE 2>>$LOG_FILE; then
-        return 1
-    fi
+# install aur packages using PKGBUILD
+install_aur() {
+    echo -e "\t"
+    echo -e "\t------- Installing AUR Packages --------"
+    echo -e "\t"
+    for pac in "${PACKAGES_AUR[@]}"; do
+        # extract the package name from the aur link
+        AUR_PAC_NAME="$(echo $pac | cut -d / -f 4 | cut -d . -f 1)"
+        echo -ne "${INDENT}${COL_DEFAULT}${ICO_DEF}${SPACER}${COL_DEFAULT}${AUR_PAC_NAME}${COL_DEFAULT}"
+        if (
+            useradd kiss &&
+                su kiss -c "
+                git clone "$pac" /tmp/${AUR_PAC_NAME} &&
+                cd /tmp/${AUR_PAC_NAME} &&
+                makepkg -s &&
+                exit &&"
+            userdel kiss &&
+                pacman --root ${ROOT} -U *.pkg.tar.xz
+        ); then
+            echo -ne "\r"
+            echo -e "${INDENT}${GREEN}${ICO_OK}${SPACER}${COL_DEFAULT}${AUR_PAC_NAME}${COL_DEFAULT}"
+        else
+            echo -ne "\r"
+            echo -e "${INDENT}${RED}${ICO_ERR}${SPACER}${COL_DEFAULT}${AUR_PAC_NAME}${COL_DEFAULT}"
+            echo -e "${RED}Installation failed at the above stage${COL_DEFAULT}"
+            exit 1
+        fi
+    done
 }
 
-#----------------------------------------------------------------
-#
-#                        HERE BE DRAGONS !!!
-#
-#----------------------------------------------------------------
+# ---------------------------------------------------------------------------- #
+#                              HERE BE DRAGONS !!!                             #
+#                     avoid this mess as much as possible,                     #
+#                   if you can clean it up knock yourself out                  #
+# ---------------------------------------------------------------------------- #
 
-# avoid this mess as much as possible, if you can clean it up knock yourself out
 main() {
-    # create a logfile and makesure everyone and read/write
-    touch $LOG_FILE && chmod 666 $LOG_FILE
+    # delete the log file if it exists alread
+    rm -f $LOG_FILE
+    # create a logfile and makesure everyone can read/write
+    touch $LOG_FILE && chmod 777 $LOG_FILE
 
     for ((n = 0; n < ${#STAGES[@]}; n += 2)); do
         # make a note (in the log file) of where we are now
         if (($((n % 2)) == 0)); then
-            # echo -ne "${LOG_DELIMITER}\n# \n${LOG_DELIMITER}" >>$LOG_FILE
             echo -e "-----------------------------------------------------------" >>$LOG_FILE
             echo -e " ${STAGES[$n]}                                             " >>$LOG_FILE
             echo -e "-----------------------------------------------------------" >>$LOG_FILE
         fi
 
         # if the stage has a description procced with status update
-        if [[ ! -z ${STAGES[$((n + 1))]} ]]; then
-            echo -ne "${INDENT}${WHITE}${ICO_DEF}${SPACER}${STAGES[$((n + 1))]}${COL_DEFAULT}"
+        if [[ -n ${STAGES[$((n + 1))]} ]]; then
+            echo -ne "${INDENT}${COL_DEFAULT}${ICO_DEF}${SPACER}${STAGES[$((n + 1))]}${COL_DEFAULT}"
             if ${STAGES[$n]}; then
                 echo -ne "\r"
-                echo -e "${INDENT}${GREEN}${ICO_OK}${SPACER}${WHITE}${STAGES[$((n + 1))]}${COL_DEFAULT}"
-                echo -e ">> no errors encountered\n" >>$LOG_FILE
+                echo -e "${INDENT}${GREEN}${ICO_OK}${SPACER}${COL_DEFAULT}${STAGES[$((n + 1))]}${COL_DEFAULT}"
+                echo -e "==> no errors encountered\n" >>$LOG_FILE
             else
                 echo -ne "\r"
-                echo -e "${INDENT}${RED}${ICO_ERR}${SPACER}${WHITE}${STAGES[$((n + 1))]}${COL_DEFAULT}"
-                echo -e "${RED}Installation failed at the above stage${COL_DEFAULT}"
+                echo -e "${INDENT}${RED}${ICO_ERR}${SPACER}${COL_DEFAULT}${STAGES[$((n + 1))]}${COL_DEFAULT}"
+                echo -e "${RED}Installation failed at the above stage.${COL_DEFAULT}"
+                echo -e "${RED}Take a look at the log here: $LOG_FILE${COL_DEFAULT}"
                 exit 1
             fi
         else
