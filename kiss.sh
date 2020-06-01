e5a191f048fb3a4c6d242cf928efbae15c1ee35d
Kamyab Sherafat <k4my4b@gmail.com>
2020-06-01 17:25:21 +0100

    added: stage to install git

--- kiss.sh
+++ kiss.sh
@@ -18,7 +18,8 @@ STAGES=(
     check_connection "Checking connection ..."
     # update pacman repos
     update_pacman "Updating pacman ..."
-    # install reflector to update our mirrorlist, we don't want the installation to take forever
+    # install reflector to update our mirrorlist,
+    # we don't want the installation to take forever
     install_reflector "Installing reflector ..."
     # install whois, we need this for the next step
     install_whois "Installing whois"
@@ -37,7 +38,10 @@ STAGES=(
     install_fakeroot "Installing fakeroot ..."
     # install binutils needed for compiling aur pacakges
     install_binutils "Installing binutils ..."
-    # add the required installer user otherwise we can't compile and install aur packages
+    # install git, we need this to retrieve aur packages
+    install_git "Installing git ..."
+    # add the required installer user otherwise we can't
+    # compile and install aur packages
     useradd_kiss "Adding ArchKISS user ..."
     # install a AUR packages
     install_aur ""
@@ -152,75 +156,77 @@ print_logo() {
 
 # make sure we have root privilege
 check_root() {
-    if ! [ "$(id -u)" = 0 ] 1>>$LOG_FILE 2>>$LOG_FILE; then
+    if ! [ "$(id -u)" = 0 ] 1>>${LOG_FILE} 2>>${LOG_FILE}; then
         return 1
     fi
 }
 
 # make sure we are connected
 check_connection() {
-    if ! ping -c 1 google.com 1>>$LOG_FILE 2>>$LOG_FILE; then
+    if ! ping -c 1 google.com 1>>${LOG_FILE} 2>>${LOG_FILE}; then
         return 1
     fi
 }
 
 # get pacman started
 update_pacman() {
-    if ! pacman -Syy 1>>$LOG_FILE 2>>$LOG_FILE; then
+    if ! pacman -Sy 1>>${LOG_FILE} 2>>${LOG_FILE}; then
         return 1
     fi
 }
 
 # install reflector to get the mirrorlist updated, we don't want this installation to take forever
 install_reflector() {
-    if ! pacman --needed --noconfirm -S reflector 1>>$LOG_FILE 2>>$LOG_FILE; then
+    if ! pacman --needed --noconfirm -S reflector 1>>${LOG_FILE} 2>>${LOG_FILE}; then
         return 1
     fi
 }
 
 # install whois package we need this for the next step
 install_whois() {
-    if ! pacman --needed --noconfirm -S whois 1>>$LOG_FILE 2>>$LOG_FILE; then
+    if ! pacman --needed --noconfirm -S whois 1>>${LOG_FILE} 2>>${LOG_FILE}; then
         return 1
     fi
 }
 
 # find location/country of user to pass to reflector
 find_country() {
-    if ! COUNTRY="$(whois "$(curl -sSL ifconfig.me)" | grep -iE -m1 ^country: | cut -d ' ' -f9)" 1>>$LOG_FILE 2>>$LOG_FILE; then
+    if ! COUNTRY="$(whois "$(curl -sSL ifconfig.me)" | grep -iE -m1 ^country: | cut -d ' ' -f9)" 1>>${LOG_FILE} 2>>${LOG_FILE}; then
         return 1
     fi
 }
 
 # with a bit of luck we should we able to find some better mirrors around
 update_mirrors() {
-    if ! reflector -c "${COUNTRY}" --sort score --threads "$(nproc)" --save $MIRRORS_URI 1>>$LOG_FILE 2>>$LOG_FILE; then
+    if ! reflector -c "${COUNTRY}" --sort score --threads "$(nproc)" --save $MIRRORS_URI 1>>${LOG_FILE} 2>>${LOG_FILE}; then
         return 1
     fi
 }
 
 # enable network time sync
 enable_ntp() {
-    if ! timedatectl set-ntp true 1>>$LOG_FILE 2>>$LOG_FILE; then
+    if ! timedatectl set-ntp true 1>>${LOG_FILE} 2>>${LOG_FILE}; then
         return 1
     fi
 }
 
 # install the base system
 install_base() {
-    if ! pacstrap "${ROOT}" "${PACKAGES_BASE[@]}" 1>>$LOG_FILE 2>>$LOG_FILE; then
+    if ! pacstrap "${ROOT}" "${PACKAGES_BASE[@]}" 1>>${LOG_FILE} 2>>${LOG_FILE}; then
         return 1
     fi
 }
 
-# install the rest of the packges this doesm't mean that these are not essential
+# install the packages we need for a properly usable system
+# these are in the official repository so we don't have to
+# compile them
 install_pac() {
     echo -e "\t"
     echo -e "\t----- Installing Official Packages -----"
     echo -e "\t"
     for pac in "${PACKAGES[@]}"; do
         echo -ne "${INDENT}${COL_DEFAULT}${ICO_DEF}${SPACER}${COL_DEFAULT}$pac${COL_DEFAULT}"
-        if pacman --needed --noconfirm --root "${ROOT}" -S "$pac" 1>>$LOG_FILE 2>>$LOG_FILE; then
+        if pacman --needed --noconfirm --root "${ROOT}" -S "$pac" 1>>${LOG_FILE} 2>>${LOG_FILE}; then
             echo -ne "\r"
             echo -e "${INDENT}${GREEN}${ICO_OK}${SPACER}${COL_DEFAULT}$pac${COL_DEFAULT}"
         else
@@ -237,14 +243,21 @@ install_pac() {
 
 # install fakeroot package we need this for the next step
 install_fakeroot() {
-    if ! pacman --needed --noconfirm -S fakeroot 1>>$LOG_FILE 2>>$LOG_FILE; then
+    if ! pacman --needed --noconfirm -S fakeroot 1>>${LOG_FILE} 2>>${LOG_FILE}; then
         return 1
     fi
 }
 
 # install binutils package we need this for the next step
 install_binutils() {
-    if ! pacman --needed --noconfirm -S binutils 1>>$LOG_FILE 2>>$LOG_FILE; then
+    if ! pacman --needed --noconfirm -S binutils 1>>${LOG_FILE} 2>>${LOG_FILE}; then
+        return 1
+    fi
+}
+
+# install git, we need to pull in the aur packages using git
+install_git() {
+    if ! pacman --needed --noconfirm -S git 1>>${LOG_FILE} 2>>${LOG_FILE}; then
         return 1
     fi
 }
@@ -253,7 +266,10 @@ install_binutils() {
 # is neccessary for compiling aur package (none-root) and installing
 # dependencies (root)
 useradd_kiss() {
-    if ! useradd "${ARCHKISS_USER}" && echo "${ARCHKISS_USER} ALL=(ALL) NOPASSWD:ALL" >/etc/sudoers.d/${ARCHKISS_USER} 1>>$LOG_FILE 2>>$LOG_FILE; then
+    if ! (
+        useradd "${ARCHKISS_USER}" &&
+            echo "${ARCHKISS_USER} ALL=(ALL) NOPASSWD:ALL" >/etc/sudoers.d/${ARCHKISS_USER}
+    ) 1>>${LOG_FILE} 2>>${LOG_FILE}; then
         return 1
     fi
 }
@@ -268,9 +284,9 @@ install_aur() {
         AUR_PAC_NAME="$(echo ${pac} | cut -d / -f 4 | cut -d . -f 1)"
         echo -ne "${INDENT}${COL_DEFAULT}${ICO_DEF}${SPACER}${COL_DEFAULT}${AUR_PAC_NAME}${COL_DEFAULT}"
         if (
-            su kiss -c "git clone "${pac}" ${ROOT}/tmp/${AUR_PAC_NAME} && cd ${ROOT}/tmp/${AUR_PAC_NAME} && MAKEFLAGS="-j$(nproc)" makepkg -s --noconfirm --needed"
+            su "${ARCHKISS_USER}" -c "git clone "${pac}" ${ROOT}/tmp/${AUR_PAC_NAME} && cd ${ROOT}/tmp/${AUR_PAC_NAME} && MAKEFLAGS="-j$(nproc)" makepkg -s --noconfirm --needed"
             pacman --noconfirm --needed --root "${ROOT}" -U ${ROOT}/tmp/${AUR_PAC_NAME}/*.pkg.tar.xz
-        ) 1>>$LOG_FILE 2>>$LOG_FILE; then
+        ); then
             echo -ne "\r"
             echo -e "${INDENT}${GREEN}${ICO_OK}${SPACER}${COL_DEFAULT}${AUR_PAC_NAME}${COL_DEFAULT}"
         else
@@ -293,16 +309,16 @@ install_aur() {
 
 main() {
     # delete the log file if it exists alread
-    rm -f $LOG_FILE
+    rm -f ${LOG_FILE}
     # create a logfile and makesure everyone can read/write
-    touch $LOG_FILE && chmod 777 $LOG_FILE
+    touch ${LOG_FILE} && chmod 777 ${LOG_FILE}
 
     for ((n = 0; n < ${#STAGES[@]}; n += 2)); do
         # make a note (in the log file) of where we are now
         if (($((n % 2)) == 0)); then
-            echo -e "-----------------------------------------------------------" >>$LOG_FILE
-            echo -e " ${STAGES[$n]}                                             " >>$LOG_FILE
-            echo -e "-----------------------------------------------------------" >>$LOG_FILE
+            echo -e "-----------------------------------------------------------" >>${LOG_FILE}
+            echo -e " ${STAGES[$n]}                                             " >>${LOG_FILE}
+            echo -e "-----------------------------------------------------------" >>${LOG_FILE}
         fi
 
         # if the stage has a description procced with status update
@@ -311,12 +327,12 @@ main() {
             if ${STAGES[$n]}; then
                 echo -ne "\r"
                 echo -e "${INDENT}${GREEN}${ICO_OK}${SPACER}${COL_DEFAULT}${STAGES[$((n + 1))]}${COL_DEFAULT}"
-                echo -e "==> no errors encountered\n" >>$LOG_FILE
+                echo -e "==> no errors encountered\n" >>${LOG_FILE}
             else
                 echo -ne "\r"
                 echo -e "${INDENT}${RED}${ICO_ERR}${SPACER}${COL_DEFAULT}${STAGES[$((n + 1))]}${COL_DEFAULT}"
                 echo -e "${RED}Installation failed at the above stage.${COL_DEFAULT}"
-                echo -e "${RED}Take a look at the log here: $LOG_FILE${COL_DEFAULT}"
+                echo -e "${RED}Take a look at the log here: ${LOG_FILE}${COL_DEFAULT}"
                 exit 1
             fi
         else
